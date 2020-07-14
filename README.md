cabf0c0fbc8ee4807da723afffe6b0713de4691b
OYKnitter <ok@knitter.us>
2020-07-13 22:31:18 -0700

    Updates to README and shell script.

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # QAOA---Ising-Model
 
-This code expands on work done by Tianchen Zhao, Giuseppe Carleo, James Stokes, and Shravan Veerapaneni, as may be seen here: https://github.com/Ericolony/QAOA
+This code expands on a program written by Tianchen Zhao, Giuseppe Carleo, James Stokes, and Shravan Veerapaneni, which may be seen here: https://github.com/Ericolony/QAOA
+
+So far this code is able to solve MaxCut and Ising Model QAOA problems using the Python package NetKet.
--- SpinGlassScript.sh
+++ SpinGlassScript.sh
@@ -1,1 +1,11 @@
-python spinglasstest.py --learning_rate=0.01 --pb_type="spinglass" --batch_size=2048 --model_name="rbm" --param_init=0.01 --input_size=9 --optimizer="sgd" --num_of_iterations=100 --use_sr=True --random_example=True
+python spinglasstest.py --learning_rate=0.01 --pb_type="spinglass" --batch_size=128 --model_name="rbm" --param_init=0.01 --input_size=100 --optimizer="sgd" --num_of_iterations=500 --use_sr=True --random_example=False
+
+python spinglasstest.py --learning_rate=0.01 --pb_type="spinglass" --batch_size=256 --model_name="rbm" --param_init=0.01 --input_size=100 --optimizer="sgd" --num_of_iterations=500 --use_sr=True --random_example=False
+
+python spinglasstest.py --learning_rate=0.01 --pb_type="spinglass" --batch_size=512 --model_name="rbm" --param_init=0.01 --input_size=100 --optimizer="sgd" --num_of_iterations=500 --use_sr=True --random_example=False
+
+python spinglasstest.py --learning_rate=0.01 --pb_type="spinglass" --batch_size=1024 --model_name="rbm" --param_init=0.01 --input_size=100 --optimizer="sgd" --num_of_iterations=500 --use_sr=True --random_example=False
+
+python spinglasstest.py --learning_rate=0.01 --pb_type="spinglass" --batch_size=2048 --model_name="rbm" --param_init=0.01 --input_size=100 --optimizer="sgd" --num_of_iterations=500 --use_sr=True --random_example=False
+
+python spinglasstest.py --learning_rate=0.01 --pb_type="spinglass" --batch_size=4096 --model_name="rbm" --param_init=0.01 --input_size=100 --optimizer="sgd" --num_of_iterations=500 --use_sr=True --random_example=False
